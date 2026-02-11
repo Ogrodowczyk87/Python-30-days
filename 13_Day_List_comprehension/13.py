d05998426d9ffd555ebcfd1568bdd497f8da78e0
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2026-02-11 23:08:20 +0000

    update

--- 13_Day_List_comprehension/13.py
+++ 13_Day_List_comprehension/13.py
@@ -1,1 +1,19 @@
-print('hello world')
+language = "Python"
+lst = list(language)
+print(lst)
+
+lst = [i for i in language]
+print(type(lst))
+print(lst)
+
+lst = [i for i in language if i.isupper()
+       if i.isalpha()
+       if i not in "AEIOU"
+       if i not in "aeiou"
+       if i not in " "]
+print(type(lst))
+print(lst)
+
+numbers = [i for i in range(1, 11)]
+print(type(numbers))
+print(numbers)
