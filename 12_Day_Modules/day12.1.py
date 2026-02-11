ce917dc86fa67e8614f16c9890419a40f32ca983
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2026-02-11 22:36:21 +0000

    update

--- 12_Day_Modules/day12.1.py
+++ 12_Day_Modules/day12.1.py
@@ -1,8 +1,11 @@
 from day12 import generate_full_name
-
+from day12 import generate_random_user_id
 
 first_name = 'Asabeneh'
 last_name = 'Yetayeh'   
 
 full_name = generate_full_name(first_name, last_name)
 print(full_name)
+
+user_id = generate_random_user_id()
+print(user_id)
--- 12_Day_Modules/day12.py
+++ 12_Day_Modules/day12.py
@@ -1,5 +1,13 @@
-def generate_full_name(first_name, last_name):
-    space = '   '
-    full_name = first_name + space + last_name
-    return full_name
+import random
+import string
 
+
+def generate_full_name(first_name, last_name):
+    return f"{first_name} {last_name}"
+
+
+def generate_random_user_id(length=126):
+    # chars = string.ascii_letters + string.digits
+    # return "".join(random.choice(chars) for _ in range(length))
+    chars = string.ascii_letters + string.digits
+    return "".join(random.choice(chars) for _ in range(length))
