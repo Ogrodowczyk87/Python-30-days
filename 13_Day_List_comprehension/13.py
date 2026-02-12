0c7aeda6eb7a2fe72a52d2d43cd4cb0d69b9c35f
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2026-02-12 17:02:42 +0000

    update

--- 13_Day_List_comprehension/13.py
+++ 13_Day_List_comprehension/13.py
@@ -1,19 +1,39 @@
-language = "Python"
-lst = list(language)
-print(lst)
+# language = "Python"
+# lst = list(language)
+# print(lst)
 
-lst = [i for i in language]
-print(type(lst))
-print(lst)
+# lst = [i for i in language]
+# print(type(lst))
+# print(lst)
 
-lst = [i for i in language if i.isupper()
-       if i.isalpha()
-       if i not in "AEIOU"
-       if i not in "aeiou"
-       if i not in " "]
-print(type(lst))
-print(lst)
+# lst = [i for i in language if i.isupper()
+#        if i.isalpha()
+#        if i not in "AEIOU"
+#        if i not in "aeiou"
+#        if i not in " "]
+# print(type(lst))
+# print(lst)
 
-numbers = [i for i in range(1, 11)]
-print(type(numbers))
-print(numbers)
+# numbers = [i for i in range(1, 11)]
+# print(type(numbers))
+# print(numbers)
+
+
+# lenguage = "Ogrodowczyk + Python Developer"
+# lst  = list(lenguage)
+# print(lst)
+
+# lst = [i for i in lenguage if i.isupper()
+#        if i.isalpha() if i not in "AEIOU" if i not in "aeiou" if i not in " "] 
+# print(type(lst)) 
+# print(lst)
+
+
+def add_two_nums(a, b):
+       return a + b
+
+print (add_two_nums(2, 3))
+
+add_two_nums = lambda a, b: a + b
+print (add_two_nums(2, 3))
+
