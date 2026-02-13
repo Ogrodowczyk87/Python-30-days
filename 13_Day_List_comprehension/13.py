c46e1ba113b06e7355bbd1aa05ef0a6f835c2603
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2026-02-13 01:30:44 +0000

    update

--- 13_Day_List_comprehension/13.py
+++ 13_Day_List_comprehension/13.py
@@ -29,11 +29,49 @@
 # print(lst)
 
 
-def add_two_nums(a, b):
-       return a + b
+# def add_two_nums(a, b):
+#        return a + b
 
-print (add_two_nums(2, 3))
+# print (add_two_nums(2, 3))
 
-add_two_nums = lambda a, b: a + b
-print (add_two_nums(2, 3))
+# add_two_nums = lambda a, b: a + b
+# print (add_two_nums(2, 3))
 
+# def power(x):
+#        return lambda n: x ** n
+
+# cube = power(3)(4)
+# print(cube)
+
+# two_power_of_five = power(5)(2)
+# print(two_power_of_five)
+
+# numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
+# list = [i for i in numbers 
+#                     if i < 0]
+# print(list)
+
+# list_of_lists =[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# # all_numbers = [i for sublist in list_of_lists for i in sublist]
+# # print(all_numbers)
+
+# def flat_list(lst):
+#     result = []
+#     for item in lst:
+#         if isinstance(item, list):
+#             result.extend(flat_list(item))
+#         else:
+#             result.append(item)
+#     return result
+    
+# print(flat_list(list_of_lists))
+
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+
+result = [
+    {'country': country.upper(), 'city': city.upper()}
+    for [(country, city)] in countries
+]
+
+print(result)
